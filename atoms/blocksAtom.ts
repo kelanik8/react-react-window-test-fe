8019e38b6ff0c610decc5ae1b99b0a64f0a6ca6e
Kelani Tolulope <kelanik8@gmail.com>
2022-06-03 01:26:29 +0100

    feat: block list factory data and function

--- atoms/blocksAtom.ts
+++ atoms/blocksAtom.ts
@@ -1,6 +1,6 @@
 import { atom } from "recoil";
 
-export const blocksListState = atom({
+export const blocksListsAtom = atom({
   key: "blocksListState",
   default: [],
 });
--- components/AppAside/index.tsx
+++ components/AppAside/index.tsx
@@ -1,11 +1,15 @@
+import { useRecoilValue } from "recoil";
 import { AppLogo, MenuLinkItem } from "..";
+import { blocksListsAtom } from "../../atoms/blocksAtom";
 import { MenuContainer, MenuNavigator } from "./styles";
 
 export default function AppAside() {
+  const blocksLists = useRecoilValue(blocksListsAtom);
   return (
     <MenuContainer>
       <MenuNavigator>
         <AppLogo />
+        {JSON.stringify(blocksLists)}
         <MenuLinkItem text="Link 1" />
         <MenuLinkItem text="Link 2" />
         <MenuLinkItem text="Link 3" />
