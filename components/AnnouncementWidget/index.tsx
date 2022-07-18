73cc6530866c0890b4153132f9ac8954d4ec3983
Kelani Tolulope <kelanik8@gmail.com>
2022-07-18 10:04:28 +0100

    Update index.tsx

--- components/AnnouncementWidget/index.tsx
+++ components/AnnouncementWidget/index.tsx
@@ -3,7 +3,7 @@ import { Announcement, AnnouncementContainer } from "./styles";
 export default function AnnouncementWidget() {
   return (
     <AnnouncementContainer>
-      <Announcement>Plancraft Front-end Test</Announcement>
+      <Announcement>-</Announcement>
     </AnnouncementContainer>
   );
 }
